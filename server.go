46c7dc16fced8302c7ed90b07501bbaef1d3f24c
jon-rod21 <jonathan.rodriguez9921@gmail.com>
2026-09-23 03:03:05 -0500

    need to populate website from db

--- server.go
+++ server.go
@@ -7,16 +7,30 @@ import (
 	"encoding/json"
 	"os"
 	"strings"
+	"database/sql"
+	_ "modernc.org/sqlite"
 ) 
 
 func main() {
+
+	db, err := sql.Open("sqlite", "./media.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	syncMediaFolder(db)
+
+
 	fs := http.FileServer(http.Dir("."))
 	http.Handle("/", fs);
 
 	http.HandleFunc("/api/library", func (w http.ResponseWriter, r *http.Request) {
-		entries, err := os.ReadDir("./media")
+		rows, err := db.Query("SELECT media_id, title, filename FROM media")
+		
 		if err != nil {
-			http.Error(w, "could not read media folder", http.StatusInternalServerError)
+
+			log.Println("query failed for media", )
 			return
 		}
 
@@ -38,3 +52,23 @@ func main() {
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
+// populate the db with new files
+func syncMediaFolder(db *sql.DB) {
+	entries, err := os.ReadDir("./media")
+	if err != nil {
+		log.Println("could not read media folder:", err)
+		return
+	}
+
+	for _, e := range entries {
+		filename := e.Name()
+
+		_, err := db.Exec(
+			'INSERT OR IGNORE INTO media (title, filename) VALUES (?, ?)',
+			filename, filename,
+		)
+		if err != nil {
+			log.Println("insert failed for", filename, ":", err)
+		}
+	}
+}
